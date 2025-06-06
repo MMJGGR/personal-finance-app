956243e257b76354de3f682bfc4b06c648d72c61
MMJGGR <87863735+MMJGGR@users.noreply.github.com>
2025-06-06 23:09:59 +0300

    chore: consolidate Jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,5 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest'
-  }
+  },
+  moduleFileExtensions: ['js', 'jsx', 'json'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom']
 }
