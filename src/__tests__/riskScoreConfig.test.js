2b38889134eb97d085c29f8715c2530e4bf8e609
MMJGGR <87863735+MMJGGR@users.noreply.github.com>
2025-07-02 12:27:19 +0300

    Remove unused risk score config

--- src/__tests__/riskScoreConfig.test.js
+++
@@ -1,7 +0,0 @@
-/* global test, expect */
-import { riskScoreMap } from '../riskScoreConfig'
-
-test('riskScoreMap exposes expected categories', () => {
-  const keys = Object.keys(riskScoreMap).sort()
-  expect(keys).toEqual(['goal', 'horizon', 'knowledge', 'response'].sort())
-})
--- src/riskScoreMap.js
+++
@@ -1,5 +0,0 @@
-export const riskScoreMap = {
-  conservative: { min: 0, max: 30 },
-  balanced: { min: 31, max: 70 },
-  growth: { min: 71, max: 100 }
-}
